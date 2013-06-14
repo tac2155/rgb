332c72ee659842dec55b91a010d46b0d34043c5e
tac2155 <tcousins2010@gmail.com>
2013-06-14 15:50:31 -0500

    basic serial send

--- firmware/rgb.cpp
+++ firmware/rgb.cpp
@@ -41,16 +41,24 @@ void USART_Init( unsigned int ubrr) {
 	UBRR0H = (unsigned char)(ubrr>>8); 
 	UBRR0L = (unsigned char)ubrr;
 	// Enable receiver 
-	UCSR0B = (1<<RXEN0);
+	UCSR0B = (1<<RXEN0) | (1<<TXEN0);
 	/* Set frame format: 8 data, 1 stop bit */ 
 	UCSR0C = ((1 << UCSZ01) | (1 << UCSZ00));
 }
 
+/*
 unsigned char USART_Receive( void ) {
-	/* Wait for data to be received */ 
+	// Wait for data to be received 
 	while ( !(UCSR0A & (1<<RXC0)) );
-	/* Get and return received data from buffer */ 
+	// Get and return received data from buffer  
 	return UDR0;
+} */
+
+void USART_Transmit( unsigned char data ) {
+	//Wait for empty transmit buffer  
+	while ( !( UCSR0A & (1<<UDRE0)) );
+	//Put data into buffer, sends the data */ 
+	UDR0 = data;
 }
 
 ISR(USART_RX_vect) {
@@ -59,6 +67,7 @@ ISR(USART_RX_vect) {
 
 	if(data == addr) {
 		lightShow.setEffect(2);
+		USART_Transmit(data);
 	}
 	else {
 		lightShow.setEffect(data);
